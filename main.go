6d48d4b5ed6e51963fa23b752720b9c5b84afc76
tarunz <tarunk10@hotmail.com>
2019-03-23 13:55:27 +0100

    Add bin scripts and main processString function.

--- main.go
+++ main.go
@@ -1,31 +1,77 @@
 package main
 
 import (
-	pk "./src/parkinglot"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	pk "./src/parkingLot"
 )
 
 func main() {
-	// c := pk.NewCar("123", "Black")
-	// c.SetColor("G")
 
-	p := pk.NewParkingLot(6)
-	// p.Cars[2] = pk.MakeCar("123", pk.Orange)
-	// p.Park("123", "Black", 1)
-	p.Park("KA-01-HH-1234", "White")
-	p.Park("KA-01-HH-9999", "White")
-	p.Park("KA-01-BB-0001", "Black")
-	p.Park("KA-01-HH-7777", "Red")
-	p.Park("KA-01-HH-2701", "Blue")
-	p.Park("KA-01-HH-3141", "Black")
-	p.Leave(4)
-	p.Status()
-	p.Park("KA-01-P-333", "White")
-	p.Park("DL-12-AA-9999", "White")
-	p.GetRegNoOfColor("White")
-	p.GetSlotNoOfColor("White")
-	p.GetSlotNoOfCar("KA-01-HH-3141")
-	p.GetSlotNoOfCar("MH-04-AY-1111")
-	// fmt.Println(p)
-	// p.Leave(1)
-	// fmt.Println(p)
+	arg := os.Args[1:]
+
+	if len(arg) > 0 {
+		// File Mode
+		file, err := os.Open(arg[0])
+		if err != nil {
+			fmt.Printf("File does not exist: %s\n", arg[0])
+		} else {
+			defer file.Close()
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				// fmt.Println(scanner.Text())
+				processString(scanner.Text())
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err)
+			}
+		}
+	} else {
+		// Interactive Mode
+		for {
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			text := scanner.Text()
+			if text == "exit" {
+				break
+			}
+			processString(text)
+		}
+	}
+}
+
+var plot *pk.ParkingLot
+
+func processString(s string) {
+	vals := strings.Split(s, " ")
+	switch vals[0] {
+	case "create_parking_lot":
+		n, err := strconv.Atoi(vals[1])
+		if err != nil {
+			fmt.Println("CREATE: Invalid value for number of slots.")
+		}
+		plot = pk.NewParkingLot(n)
+	case "park":
+		plot.Park(vals[1], vals[2])
+	case "leave":
+		i, err := strconv.Atoi(vals[1])
+		if err != nil {
+			fmt.Println("LEAVE: Invalid value for slot number.")
+		}
+		plot.Leave(i)
+	case "status":
+		plot.Status()
+	case "registration_numbers_for_cars_with_colour":
+		plot.GetRegNoOfColor(vals[1])
+	case "slot_numbers_for_cars_with_colour":
+		plot.GetSlotNoOfColor(vals[1])
+	case "slot_number_for_registration_number":
+		plot.GetSlotNoOfCar(vals[1])
+	default:
+		fmt.Println("ACTION: Invalid action")
+	}
 }
